d81a2ca70e320ab1d344762d005867f1b0ee94ed
jordan luce <jordan.luce972@gmail.com>
2018-01-29 19:05:46 +0000

    added lesson 1

---
+++ basics/basics.rb
@@ -0,0 +1,43 @@
+## STRING
+
+# puts 'hello world!'
+# puts "".class
+# puts ''.class
+
+# puts 'hello world'.gsub('hello', 'my')
+# puts 'this is my string'.include?('string')
+# puts 'this is my string'.split.size
+# puts 'this is my string'.size.class
+
+## SYMBOL
+
+# puts :hello
+# puts :'hello'
+# puts :hello.class
+
+# puts 'world'.object_id
+# puts 'world'.object_id
+
+# puts :world.object_id
+# puts :world.object_id
+
+## INTEGER
+
+# puts 5.class
+# puts '5'.class
+# puts 5.odd?
+# puts 4.odd?
+# puts 5 + 5
+
+## NIL
+
+# puts nil.class
+# puts "hello".index('e')
+# puts "hello".index('z').class
+# puts nil.count
+
+## TYPE CONVERSION
+
+# puts 'hello'.to_sym.class
+# puts '1'.to_i.class
+# puts '5'.to_f
