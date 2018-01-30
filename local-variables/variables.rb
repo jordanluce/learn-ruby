bda1144c1c0af40d9a924312c165b20f0316ce47
jordan luce <jordan.luce972@gmail.com>
2018-01-30 12:07:21 +0000

    completed lesson 2

---
+++ local-variables/variables.rb
@@ -0,0 +1,17 @@
+# LESSON 2
+
+
+ # firstname = 'kingsley'
+ # lastname = 'ijomah'
+
+# puts firstname
+# puts lastname
+
+# a = 5
+# b = 10
+
+# puts a + b 
+
+## Variables within string
+
+# puts "my name is #{firstname} #{lastname}"
