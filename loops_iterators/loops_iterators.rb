43f5508d2e6a7c96cedb3a5c41248eac6cce26d9
jordan luce <jordan.luce972@gmail.com>
2018-01-31 11:32:38 +0000

    completed lesson 5

---
+++ loops_iterators/loops_iterators.rb
@@ -0,0 +1,60 @@
+## infinite loop
+
+# loop do 
+# 	puts 'I will go forever'
+# end
+
+
+## while loop
+
+# puts 'enter number below:'
+# x = gets.chomp.to_i
+
+# puts '-----result-----'
+# while x >= 0
+# 	puts x 
+# 	x = x - 1
+# end
+
+
+## until loop
+
+# puts 'enter number below:'
+# x = gets.chomp.to_i
+
+# puts '-----result-----'
+# until x < 0 
+# 	puts x
+# 	x -= 1
+# end
+
+
+## for loop
+
+# puts 'enter number below:'
+# x = gets.chomp.to_i
+
+# for i in 1..x do
+# 	puts i 
+# end
+
+
+## each iterator
+
+# result = [1,2,3,4,5].each {|n| n * 2}
+
+# puts result.inspect
+
+#----------
+
+# result = [1,2,3,4,5].each {|n| puts n * 2}
+
+
+## map iterator
+
+# result = [1,2,3,4,5].map{|n| n * 2}
+
+# puts result.inspect
+
+
+
