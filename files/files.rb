01d6352b1b50dcb3afe837f7590104091e255228
jordan luce <jordan.luce972@gmail.com>
2018-02-04 11:45:44 +0000

    completed lesson 8

---
+++ files/files.rb
@@ -0,0 +1,16 @@
+## LESSON 8
+
+## creatiing files
+
+# my_file = File.new('todo.txt', 'w+')
+# my_file.close
+
+
+## writting into files
+
+# File.open('todo.txt', 'w') {|file| file.write('learn to code with kingsley.')}
+
+
+## reading lines
+
+# puts File.read('todo.txt')
