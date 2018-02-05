bd79df190632323ece4966a097f6141af606b97b
jordan luce <jordan.luce972@gmail.com>
2018-02-05 14:08:55 +0000

    extention lesson 9

---
+++ class/method_type_and_visibility.rb
@@ -0,0 +1,60 @@
+## Lesson 10
+
+## instance method and call method
+
+# class Customer
+# 	attr_accessor :firstname, :lastname, :email
+
+# 	def initialize (firstname, lastname, email)
+# 		@firstname = firstname
+# 		@lastname = lastname
+# 		@email = email
+# 	end
+
+# 	def to_s
+# 		"#{@firstname} #{@lastname}"
+# 	end
+
+# 	def self.status
+# 		'active'
+# 	end
+# end
+
+# puts Customer.status
+
+
+## public and private methods
+
+class Customer
+	attr_accessor :firstname, :lastname, :email
+
+	def initialize (firstname, lastname, email)
+		@firstname = firstname
+		@lastname = lastname
+		@email = email
+	end
+
+	def to_s
+		"#{@firstname} #{@lastname}"
+	end
+
+	def self.status
+		'active'
+	end
+
+	def cost
+		100
+	end
+
+	def bill
+		cost - discount
+	end
+
+	private
+
+	def discount
+		80
+	end
+end
+customer = Customer.new('john', 'doe', 'john.doe@test.com')
+puts customer.bill
